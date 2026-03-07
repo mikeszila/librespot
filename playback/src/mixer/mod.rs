7f7ba4746bd195c7094e904193274ca0ed38b93f
Michael <michael@MichaelServal.localdomain>
2026-03-07 03:07:37 -0500

    volume chatgpt

--- playback/src/mixer/mod.rs
+++ playback/src/mixer/mod.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use crate::config::VolumeCtrl;
 
 pub mod mappings;
+pub mod observablefixed;
 use self::mappings::MappedCtrl;
 
 pub struct NoOpVolume;
@@ -32,6 +33,7 @@ impl VolumeGetter for NoOpVolume {
 
 pub mod softmixer;
 use self::softmixer::SoftMixer;
+use self::observablefixed::ObservableFixedMixer;
 
 #[cfg(feature = "alsa-backend")]
 pub mod alsamixer;
@@ -65,10 +67,10 @@ fn mk_sink<M: Mixer + 'static>(config: MixerConfig) -> Arc<dyn Mixer> {
 
 pub const MIXERS: &[(&str, MixerFn)] = &[
     (SoftMixer::NAME, mk_sink::<SoftMixer>), // default goes first
+    (ObservableFixedMixer::NAME, mk_sink::<ObservableFixedMixer>),
     #[cfg(feature = "alsa-backend")]
     (AlsaMixer::NAME, mk_sink::<AlsaMixer>),
 ];
-
 pub fn find(name: Option<&str>) -> Option<MixerFn> {
     if let Some(name) = name {
         MIXERS
